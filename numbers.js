4bf9d16fba0fe076156d5bacee9e9716224cae88
Kali Correll <kalicorrell18@gmail.com>
2020-11-05 10:41:01 -0500

    Updated onClick Method

--- numbers.js
+++ numbers.js
@@ -1,4 +1,4 @@
-function loaded() {
+
 let nbrs = [
     106,    573,    204,    804,    482,
     457,    769,    889,    860,    399,
@@ -6,20 +6,23 @@ let nbrs = [
     590,    520,    321,    341,    619,
     505,    769,    532,    764,    101
 ];
-let largest = nbrs[0];
-let smallest = nbrs[0];
+function Clicked() {
+let largest = 0;
+let smallest = 1000;
 
 
-for (let i = 1; i < nbrs.length; i++) {
+for (let i = 0; i < nbrs.length; i++) {
 
   if (nbrs[i] > largest) {
     largest = nbrs[i];
   } else if (nbrs[i] < smallest) {
     smallest = nbrs[i];
   }
-  console.log(largest);
-  console.log(smallest);
+  
 }
-document.getElementById("Largest Number").value=largest;
-document.getElementById("Smallest Number").value=smallest;
+document.getElementById("LargestNumber").value=largest;
+    document.getElementById("SmallestNumber").value=smallest;
+    console.log(largest);
+    console.log(smallest);
+
 }
